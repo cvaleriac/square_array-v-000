dcb22db501e77661a9a51e8fafdcd0854a6acf9b
valeria campilongo <cvaleriac@hotmail.it>
2018-04-10 20:55:07 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,7 @@
 def square_array(array)
-  Array.new(array.size) { |i| array[i] ** 2}
-  end
+    new_array = []
+    array.each do |element|
+        new_array << element ** 2
+    end
+    new_array
 end
